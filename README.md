411c511de33d6da32678ed2b423d16bf93ff10b4
jjl <james.laver@gmail.com>
2021-07-30 16:44:21 +0200

    v1.0.0

---
+++ README.md
@@ -0,0 +1,21 @@
+# dummy-waker
+
+[![License](https://img.shields.io/crates/l/dummy-waker.svg)](https://github.com/irrustible/dummy-waker/blob/main/LICENSE)
+[![Package](https://img.shields.io/crates/v/dummy-waker.svg)](https://crates.io/crates/dummy-waker)
+[![Documentation](https://docs.rs/dummy-waker/badge.svg)](https://docs.rs/dummy-waker)
+
+Get a `core::task::Waker` that does absolutely nothing.
+
+Usage:
+
+```rust
+dummy_waker::dummy_waker()
+```
+
+## Copyright and License
+
+Copyright (c) 2021 James Laver
+
+Licensed under Apache License, Version 2.0 (https://www.apache.org/licenses/LICENSE-2.0), with LLVM Exceptions (https://spdx.org/licenses/LLVM-exception.html).
+
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be licensed as above, without any additional terms or conditions.
---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+// Copyright (c) 2021 James Laver
+// 
+// Licensed under Apache License, Version 2.0
+// (https://www.apache.org/licenses/LICENSE-2.0), with LLVM Exceptions
+// (https://spdx.org/licenses/LLVM-exception.html).
+//
+// Unless you explicitly state otherwise, any contribution
+// intentionally submitted for inclusion in the work by you, as
+// defined in the Apache-2.0 license, shal l be licensed as above,
+// without any additional terms or conditions.
+use core::task::{RawWaker, RawWakerVTable, Waker};
+
+const VTABLE: RawWakerVTable =
+    RawWakerVTable::new(
+        |data: *const ()| RawWaker::new(data, &VTABLE),
+        |_data: *const ()| (),
+        |_data: *const ()| (),
+        |_data: *const ()| (),
+    );
+    
+const RAW: RawWaker = RawWaker::new(
+    (&VTABLE as *const RawWakerVTable).cast(),
+    &VTABLE
+);
+
+/// Returns a Waker that does absolutely nothing.
+pub fn dummy_waker() -> Waker {
+    unsafe { Waker::from_raw(RAW) }
+}
